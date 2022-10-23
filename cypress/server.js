2dad211ba85dd053fdb5afd0b811106a655b15fa
brilliantdev <luckydeveloper1113@gmail.com>
2022-10-24 04:23:02 +0900

    Update server.js

--- cypress/server.js
+++ cypress/server.js
@@ -11,16 +11,3 @@ exports.start = () =>
 
         server.on('error', err => {
             if (!server.listening) {
-                return reject(err);
-            }
-
-            throw err;
-        });
-
-        const listeningServer = server.listen(8080);
-
-        // Give some time to enable error catching before resolving (for EADDRINUSE error)
-        setTimeout(() => {
-            resolve(listeningServer);
-        }, 1000);
-    });
